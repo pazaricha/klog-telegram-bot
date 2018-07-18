0bfe835ddd5a6543bfdf76ca0fb3cfac85e6d15c
Paz Aricha <paz@pazaricha.com>
2018-07-18 15:11:53 +0300

    basic api call + response

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -3,7 +3,18 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
     respond_with :message, text: t('.hi')
   end
 
-  def help!(*)
-    respond_with :message, text: "yo"
+  def message(params)
+    author_id = params['from']['id']
+    post_to_klog_server(author_id, params['text'])
+    respond_with :message, text: "I have forwarded you complaint to a CM and you will recieve a response shortly."
+    respond_with :message, text: "Cheer up :)"
+    respond_with :photo, photo: File.open("#{Rails.root}/smiling-bulldog.jpg")
   end
+
+  private
+
+  def post_to_klog_server(author_id, text)
+    RestClient.post('https://klog-staging.herokuapp.com/api/v1/complaint', { session_id: author_id, text: text }.to_json)
+  end
+
 end
